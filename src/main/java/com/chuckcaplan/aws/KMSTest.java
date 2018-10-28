f0238074005896eab0075bb5c22108d02f7e4729
ChuckCaplan <ccaplan@gmail.com>
2018-10-27 20:05:16 -0400

    readability changes

--- src/main/java/com/chuckcaplan/aws/KMSTest.java
+++ src/main/java/com/chuckcaplan/aws/KMSTest.java
@@ -7,43 +7,42 @@ import java.nio.charset.Charset;
 import java.util.Base64;
 
 public class KMSTest {
-	
-	
+
 	public static void main(String[] args) {
 		// create a client connection based on credentials
-                AWSKMS kmsClient = AWSKMSClientBuilder.defaultClient();
+		AWSKMS kmsClient = AWSKMSClientBuilder.defaultClient();
 
-// Encrypt a data key
-//
-// Replace the following fictitious CMK ARN with a valid CMK ID or ARN
-String keyId = args[0];
-ByteBuffer plaintext = str_to_bb(args[1]);
+		// Encrypt a data key
+		//
+		// Replace the following fictitious CMK ARN with a valid CMK ID or ARN
+		String keyId = args[0];
+		ByteBuffer plaintext = str_to_bb(args[1]);
 
-EncryptRequest req = new EncryptRequest().withKeyId(keyId).withPlaintext(plaintext);
-ByteBuffer ciphertext = kmsClient.encrypt(req).getCiphertextBlob();
-String b64 = Base64.getEncoder().encodeToString(ciphertext.array());
-System.out.println("Encrypted value: "+ b64);
+		EncryptRequest req = new EncryptRequest().withKeyId(keyId).withPlaintext(plaintext);
+		ByteBuffer ciphertext = kmsClient.encrypt(req).getCiphertextBlob();
+		String b64 = Base64.getEncoder().encodeToString(ciphertext.array());
+		System.out.println("Encrypted value: " + b64);
 
-// Decrypt a data key
+		// Decrypt a data key
 
-DecryptRequest dreq = new DecryptRequest().withCiphertextBlob(ByteBuffer.wrap(Base64.getDecoder().decode(b64)));
-ByteBuffer plainText = kmsClient.decrypt(dreq).getPlaintext();
-System.out.println("Decrypted value: " + bb_to_str(plainText));
+		DecryptRequest dreq = new DecryptRequest().withCiphertextBlob(ByteBuffer.wrap(Base64.getDecoder().decode(b64)));
+		ByteBuffer plainText = kmsClient.decrypt(dreq).getPlaintext();
+		System.out.println("Decrypted value: " + bb_to_str(plainText));
 	}
-	
-public static ByteBuffer str_to_bb(String msg){
-    return ByteBuffer.wrap(msg.getBytes(Charset.defaultCharset()));
-}
 
-public static String bb_to_str(ByteBuffer buffer){
-    byte[] bytes;
-    if(buffer.hasArray()) {
-        bytes = buffer.array();
-    } else {
-        bytes = new byte[buffer.remaining()];
-        buffer.get(bytes);
-    }
-    return new String(bytes, Charset.defaultCharset());
-}
+	public static ByteBuffer str_to_bb(String msg) {
+		return ByteBuffer.wrap(msg.getBytes(Charset.defaultCharset()));
+	}
+
+	public static String bb_to_str(ByteBuffer buffer) {
+		byte[] bytes;
+		if (buffer.hasArray()) {
+			bytes = buffer.array();
+		} else {
+			bytes = new byte[buffer.remaining()];
+			buffer.get(bytes);
+		}
+		return new String(bytes, Charset.defaultCharset());
+	}
 
 }
--- src/main/java/com/chuckcaplan/aws/S3Test.java
+++ src/main/java/com/chuckcaplan/aws/S3Test.java
@@ -13,39 +13,44 @@ import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
 
-public class App {
-	
+public class S3Test {
+
 	private static final String SUFFIX = "/";
-	
+
 	public static void main(String[] args) {
+
 		// create a client connection based on credentials
 		AmazonS3 s3client = AmazonS3ClientBuilder.defaultClient();
-		
+
 		// create bucket - name must be unique for all S3 users
-		String bucketName = "chuckcaplan";
-		s3client.createBucket(bucketName);
-		
-		// list buckets
-		for (Bucket bucket : s3client.listBuckets()) {
-			System.out.println(" - " + bucket.getName());
+		String bucketName = args[0];
+
+		try {
+			s3client.createBucket(bucketName);
+
+			// list buckets
+			for (Bucket bucket : s3client.listBuckets()) {
+				System.out.println(" - " + bucket.getName());
+			}
+
+			// create folder into bucket
+			String folderName = "testfolder";
+			createFolder(bucketName, folderName, s3client);
+
+			String filename = args[1];
+
+			// upload file to folder and set it to public
+			String fileName = folderName + SUFFIX + filename;
+			s3client.putObject(new PutObjectRequest(bucketName, fileName, new File(filename))
+					.withCannedAcl(CannedAccessControlList.PublicRead));
+
+			deleteFolder(bucketName, folderName, s3client);
+		} finally {
+			// deletes bucket
+			s3client.deleteBucket(bucketName);
 		}
-		
-		// create folder into bucket
-		String folderName = "testfolder";
-		createFolder(bucketName, folderName, s3client);
-		
-		// upload file to folder and set it to public
-		String fileName = folderName + SUFFIX + "Stairway to Heaven - 9:29:17, 10.14 PM.m4a";
-		s3client.putObject(new PutObjectRequest(bucketName, fileName, 
-				new File("/Users/chuck/Desktop/Stairway to Heaven - 9:29:17, 10.14 PM.m4a"))
-				.withCannedAcl(CannedAccessControlList.PublicRead));
-		
-		deleteFolder(bucketName, folderName, s3client);
-		
-		// deletes bucket
-		s3client.deleteBucket(bucketName);
 	}
-	
+
 	public static void createFolder(String bucketName, String folderName, AmazonS3 client) {
 		// create meta-data for your folder and set content-length to 0
 		ObjectMetadata metadata = new ObjectMetadata();
@@ -53,18 +58,18 @@ public class App {
 		// create empty content
 		InputStream emptyContent = new ByteArrayInputStream(new byte[0]);
 		// create a PutObjectRequest passing the folder name suffixed by /
-		PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName,
-				folderName + SUFFIX, emptyContent, metadata);
+		PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, folderName + SUFFIX, emptyContent,
+				metadata);
 		// send request to S3 to create folder
 		client.putObject(putObjectRequest);
 	}
+
 	/**
-	 * This method first deletes all the files in given folder and than the
-	 * folder itself
+	 * This method first deletes all the files in given folder and than the folder
+	 * itself
 	 */
 	public static void deleteFolder(String bucketName, String folderName, AmazonS3 client) {
-		List<S3ObjectSummary> fileList = 
-				client.listObjects(bucketName, folderName).getObjectSummaries();
+		List<S3ObjectSummary> fileList = client.listObjects(bucketName, folderName).getObjectSummaries();
 		for (S3ObjectSummary file : fileList) {
 			client.deleteObject(bucketName, file.getKey());
 		}
